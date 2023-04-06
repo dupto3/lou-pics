a9b7d1b3ac888eb73f164d370d33ede5f521e055
dupto3 <upton33@gmail.com>
2023-04-06 10:28:33 -0400

    Fixed variable names

--- js/script.js
+++ js/script.js
@@ -1,57 +1,57 @@
-const FourImage = '<img src="https://drive.google.com/uc?id=1eDpKoktcapx-xm5TSpjnKNyOm-Cp99re" />'
+const image4 = '<img src="https://drive.google.com/uc?id=1eDpKoktcapx-xm5TSpjnKNyOm-Cp99re" />'
 
-const EightImage = '<img src="https://drive.google.com/uc?id=1eK-GPynCt3XkY5uWv-6qfXtgd5JnzO-4" />'
+const image8 = '<img src="https://drive.google.com/uc?id=1eK-GPynCt3XkY5uWv-6qfXtgd5JnzO-4" />'
 
-const TwelveImage = '<img src="https://drive.google.com/uc?id=1eNfrTSrgS2TKOMweueD8LHCmJv2CvKBo" />'
+const image12 = '<img src="https://drive.google.com/uc?id=1eNfrTSrgS2TKOMweueD8LHCmJv2CvKBo" />'
 
-const SixteenImage = '<img src="https://drive.google.com/uc?id=1eRNcDOAq0Zui-Gi0sgQ5ujUew0aIZmSm" />'
+const image16 = '<img src="https://drive.google.com/uc?id=1eRNcDOAq0Zui-Gi0sgQ5ujUew0aIZmSm" />'
 
-const TwentyImage = '<img src="https://drive.google.com/uc?id=1eSyFppJRJC_nYikJUx7Fz26pFx8iXuVZ" />'
+const image20 = '<img src="https://drive.google.com/uc?id=1eSyFppJRJC_nYikJUx7Fz26pFx8iXuVZ" />'
 
-const TwentyFourImage = '<img src="https://drive.google.com/uc?id=1f-vObJAG0-lKp9cdZEBbLMVLrJ5BeHwQ" />'
+const image24 = '<img src="https://drive.google.com/uc?id=1f-vObJAG0-lKp9cdZEBbLMVLrJ5BeHwQ" />'
 
-const TwentyEightImage = '<img src="https://drive.google.com/uc?id=1eVa5KDFOz3FkAX1I2BNQh54t-MR5bac8" />'
+const image28 = '<img src="https://drive.google.com/uc?id=1eVa5KDFOz3FkAX1I2BNQh54t-MR5bac8" />'
 
-const ThirtyTwoImage = '<img src="https://drive.google.com/uc?id=1esM327Vs3tCy-65I5UXu0eEuWnrdx7_n" />'
+const image32 = '<img src="https://drive.google.com/uc?id=1esM327Vs3tCy-65I5UXu0eEuWnrdx7_n" />'
 
-const ThirtySixImage = '<img src="https://drive.google.com/uc?id=1euck95qVQNPNlhVHL2udHshsBl10iaI-" />'
+const image36 = '<img src="https://drive.google.com/uc?id=1euck95qVQNPNlhVHL2udHshsBl10iaI-" />'
 
 function setImage(imageHtml) {
     $('#bottom').empty().prepend(imageHtml)
 }
 
 $('.Four').on('click', function () {
-    setImage(FourImage)
+    setImage(image4)
 })
 
 $('.Eight').on('click', function () {
-    setImage(EightImage)
+    setImage(image8)
 })
 
 $('.Twelve').on('click', function () {
-    setImage(TwelveImage)
+    setImage(image12)
 })
 
 $('.Sixteen').on('click', function () {
-    setImage(SixteenImage)
+    setImage(image16)
 })
 
 $('.Twenty').on('click', function () {
-    setImage(TwentyImage)
+    setImage(image20)
 })
 
 $('.TwentyFour').on('click', function () {
-    setImage(TwentyFourImage)
+    setImage(image24)
 })
 
 $('.TwentyEight').on('click', function () {
-    setImage(TwentyEightImage)
+    setImage(image28)
 })
 
 $('.ThirtyTwo').on('click', function () {
-    setImage(ThirtyTwoImage)
+    setImage(image32)
 })
 
 $('.ThirtySix').on('click', function () {
-    setImage(ThirtySixImage)
+    setImage(image36)
 })
